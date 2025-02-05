14ce0e428128d1c08ecf569e7255ee31eda3f26c
Marco <126587897+MarCalll@users.noreply.github.com>
2025-02-05 16:50:44 +0100

    esercizioIncapsulamento

---
+++ Corso Python/Mercoledì05/esempiIncapsulamento.py
@@ -0,0 +1,14 @@
+class Persona:
+    __nome="Mirko"
+    
+    def getNome(self):
+        return self.__nome
+    def setNome(self,newNome):
+        self.__nome=newNome
+
+mio=Persona()
+
+#print(mio.__nome)
+print(mio.getNome())
+mio.setNome("Test")
+print(mio.getNome())
---
+++ Corso Python/Mercoledì05/esercizioIncapsulamento.py
@@ -0,0 +1,84 @@
+class Banca:
+    
+    __conti_bancari=[]
+    
+    def __init__(self,nomeBanca):
+        self.nomeBanca = nomeBanca
+    
+    def set_conti_bancari(self,new_conto_bancario):
+        self.__conti_bancari.append(new_conto_bancario)
+        
+    def get_conti_bancari(self):
+        for ele in self.__conti_bancari:
+            print(ele)
+        
+    def crea_conto_bancario(self,titolare_CB):
+        self.set_conti_bancari(ContoBancario(titolare_CB))
+        
+    def aggiungi_saldo_conto_bancario(self):
+        chi=input("A quale utente aggiungere saldo")
+        for ele in self.__conti_bancari:
+            if ele.get_titolare() == chi:
+                quanto=int(input("Quantità da aggiungere"))
+                ele.deposita(quanto)
+                
+    def preleva_da_conto_bancario(self):
+        chi=input("Da quale utente prelevare saldo?")
+        for ele in self.__conti_bancari:
+            if ele.get_titolare() == chi:
+                quanto=int(input("Quantità da prelevare"))
+                ele.preleva(quanto)
+        
+
+class ContoBancario:
+    __saldo=0
+    def __init__(self, titolare):
+        self.__titolare = titolare
+    
+    def __str__(self):
+        return f"Utente: {self.__titolare} - Saldo {self.__saldo} Euro"
+    
+    def get_titolare(self):
+        return self.__titolare
+    
+    def set_titolare(self,newTitolare):
+        self.__titolare = newTitolare
+        
+    def get_saldo(self):
+        return self.__saldo
+    
+    def set_saldo(self,newSaldo):
+        self.__saldo = newSaldo
+    
+    def deposita(self,importo):
+        if self.get_saldo()>=0:
+            self.set_saldo(self.get_saldo()+importo)
+        else:
+            print("Numero negativo! Impossibile prelevare.")
+    
+    def preleva(self,prelievo):
+        if prelievo<=self.get_saldo():
+            self.set_saldo(self.get_saldo()-prelievo)
+        else:
+            print("Impossibile prelevare, prelievo più alto del saldo di", self.get_saldo()-prelievo, "euro" )
+    
+    def visualizza_saldo(self):
+        print(self.get_saldo(), "euro depositati su questo conto.")
+      
+      
+bancadeipoveri=Banca("Banca dei poveri")
+bancadeipoveri.crea_conto_bancario("Marco")
+bancadeipoveri.crea_conto_bancario("Pippo")
+bancadeipoveri.crea_conto_bancario("Nino")
+bancadeipoveri.aggiungi_saldo_conto_bancario()
+bancadeipoveri.aggiungi_saldo_conto_bancario()
+bancadeipoveri.preleva_da_conto_bancario()
+bancadeipoveri.get_conti_bancari()
+
+#cb=ContoBancario("Marco")
+#cb.deposita(1200)
+#cb.deposita(300)
+#cb.preleva(1501)
+#cb.visualizza_saldo()
+
+        
